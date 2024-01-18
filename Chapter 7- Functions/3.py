9a07b75d931c74be8f9cba41425006b109413161
Hannah-14-python <145339759+Hannah-14-python@users.noreply.github.com>
2024-01-18 20:33:52 +0400

    AAS

---
+++ Chapter 7- Functions/3.py
@@ -0,0 +1,7 @@
+def make_shirt(size, message):
+    """Summarize the shirt that's going to be made."""
+    print("\nI'm going to make a " + size + " t-shirt.")
+    print('It will say, "' + message + '"')
+
+make_shirt('large', 'Too Cool 4 School!')
+make_shirt(message="I need to catch some zzz.", size='medium')
